70d428bdd09531ec41c41c4ed06c3e58fd9d266e
panagiotis toloudis <57294317+ptoloudis@users.noreply.github.com>
2021-10-26 22:53:11 +0300

    finish part 1in askisi 1

--- askisi1-1.c
+++ askisi1-1.c
@@ -3,3 +3,68 @@ Name: Panagiotis Toloudis
 Github: ptoloudis
 Modified on: 26 October 2021 19:37:17
 */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int fd[2],fd2[2];//File descriptor for creating a pipe
+volatile int for_exit[2];
+
+void *reader()
+{   char    ch[6];
+    int result;
+
+    read (fd[0],ch,5);
+    printf ("Reader: %s\n", ch);  
+    
+    while (for_exit[1]!=1){}
+    close(fd[0]);
+    close(fd[1]);
+    result = pipe (fd2);
+    if (result < 0){
+        perror("pipe ");
+        exit(1);
+    }
+    for_exit[0]=1;
+    write (fd2[1], ch,5);
+    printf ("\nWriter_2: %s\n", ch);
+    for (size_t i = 0; i < 1000000; i++){    }
+         
+    for_exit[0]=2;
+}
+
+void *writer()
+{   char    str[6],str2[6];
+
+    scanf("%s",str);
+    write (fd[1], str,5);
+    printf ("Writer: %s\n", str);
+    for_exit[1]=1;
+    while (for_exit[0]!=1){}
+    read (fd2[0],str2,5);
+    printf ("Reader_2: %s\n", str2); 
+    for_exit[1]=2; 
+}
+
+int main()
+{
+   pthread_t tid1,tid2;
+   int result;
+   for_exit[0]=0;
+   for_exit[1]=0;
+
+   result = pipe (fd);
+   if (result < 0){
+       perror("pipe ");
+       exit(1);
+   }
+
+   pthread_create(&tid1,NULL,reader,NULL);
+   pthread_create(&tid2,NULL,writer,NULL);
+
+   while (for_exit[1]!=2 && for_exit[0]!=2){   }
+   printf("bey\n");
+   
+}
---
+++ my_pipe.h
@@ -0,0 +1,4 @@
+int pipe_open(int size);
+int pipe_write(int p, char c);
+int pipe_writeDone(int p);
+int pipe_read(int p, char *c);
