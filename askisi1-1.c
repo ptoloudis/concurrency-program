cd0e983d3c5b22487a2288338ea6c75cb1f2451e
panagiotis toloudis <57294317+ptoloudis@users.noreply.github.com>
2021-10-27 12:47:40 +0300

    finish askisi1-1.c

--- askisi1-1.c
+++ askisi1-1.c
@@ -9,7 +9,7 @@ Modified on: 26 October 2021 19:37:17
 #include <stdio.h>
 #include <pthread.h>
 
-int fd[2],fd2[2];//File descriptor for creating a pipe
+int fd[2];//File descriptor for creating a pipe
 volatile int for_exit[2];
 
 
@@ -66,7 +66,7 @@ void *reader()
     while (for_exit[1]!=1){} 
     close(fd[0]);
     close(fd[1]);
-    result = pipe (fd2);
+    result = pipe (fd);
     if (result < 0){
         perror("pipe ");
         exit(1);
@@ -85,6 +85,7 @@ void *reader()
 
     fclose(fl); 
     for_exit[0]=2;
+    return 0;
 }
 
 void *writer()
@@ -123,6 +124,7 @@ void *writer()
         fwrite(&ch,1,1,fl);
     } 
     for_exit[1]=2; 
+    return 0;
 }
 
 int main()
@@ -143,5 +145,6 @@ int main()
 
    while (for_exit[1]!=2 && for_exit[0]!=2){   }
    printf("bey\n");
+   return 0;
    
 }
