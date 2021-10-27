7135fe29edb669a831b3b55d60990fb76a7b4fa3
panagiotis toloudis <57294317+ptoloudis@users.noreply.github.com>
2021-10-27 10:52:23 +0300

    make size

--- askisi1-1.c
+++ askisi1-1.c
@@ -12,11 +12,36 @@ Modified on: 26 October 2021 19:37:17
 int fd[2],fd2[2];//File descriptor for creating a pipe
 volatile int for_exit[2];
 
+/*int size(FILE* fl;) {
+    int j,size;
+    
+    j = fseek(fl, 0L, SEEK_END);
+    if (j == -1) {
+        ferror(fl);
+        perror("no fseek the argument file\n");
+        fclose(fl);
+        return -1;
+    }
+
+    size = ftell(fl);
+    if (size == -1) {
+        ferror(fl);
+        perror("no ftell argument file\n");
+        fclose(fl);
+        return -1;
+    }
+
+    if (size != 0) {
+        return size;
+    }
+    return 0;
+}*/
+
 void *reader()
-{   char    ch[6];
+{   char    ch[11];
     int result;
 
-    read (fd[0],ch,5);
+    read (fd[0],ch,10);
     printf ("Reader: %s\n", ch);  
     
     while (for_exit[1]!=1){}
@@ -28,7 +53,8 @@ void *reader()
         exit(1);
     }
     for_exit[0]=1;
-    write (fd2[1], ch,5);
+    
+    write (fd2[1], ch,10);
     printf ("\nWriter_2: %s\n", ch);
     for (size_t i = 0; i < 1000000; i++){    }
          
@@ -36,14 +62,15 @@ void *reader()
 }
 
 void *writer()
-{   char    str[6],str2[6];
+{   char    str[11],str2[11];
 
     scanf("%s",str);
-    write (fd[1], str,5);
+    write (fd[1], str,10);
     printf ("Writer: %s\n", str);
     for_exit[1]=1;
+    
     while (for_exit[0]!=1){}
-    read (fd2[0],str2,5);
+    read (fd2[0],str2,10);
     printf ("Reader_2: %s\n", str2); 
     for_exit[1]=2; 
 }
